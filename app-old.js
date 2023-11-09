1ce3ee875e1501fbe8dd99dad7e84767d3b80211
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-11-09 09:08:34 -0400

    Servidor con express

---
+++ app-old.js
@@ -0,0 +1,15 @@
+// creando servidor
+const http = require('http');
+// req: request (solicitud del cliente)
+// res: response (respuesta del servidor)
+// cliente: navegador, postman, etc
+const server = http.createServer((req, res) => {
+    
+    console.log('un cliente se ha conectado');
+    res.end('La conexion ha sido correcta');
+    
+});
+
+server.listen(3000, ()=>{
+  console.log('Servidor escuchando en el puerto 3000');
+})
--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // creando servidor
-const http = require('http');
-// req: request (solicitud del cliente) res: response (respuesta del servidor)
+// req: request (solicitud del cliente) 
+// res: response (respuesta del servidor)
 // cliente: navegador, postman, etc
-const server = http.createServer((req, res) => {
-    
-    console.log('un cliente se ha conectado');
-    res.end('La conexion ha sido correcta');
-    
-});
 
-server.listen(3000, ()=>{
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hola mundo')
+  console.log('un cliente se ha conectado');
+})
+
+app.listen(3000, () => {
   console.log('Servidor escuchando en el puerto 3000');
 })
